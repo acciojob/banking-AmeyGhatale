effeb7257b4af45be7d30a31d17c7543ec4a115e
“AmeyGhatale” <ameyghatale2016@gmail.com>
2024-02-28 21:57:37 +0530

    commit 5

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -29,7 +29,7 @@ public class BankAccount {
                 sum -= x;
                 ans.append(x);
                 digits--;
-                break;
+//                break;
             }
         }
 
